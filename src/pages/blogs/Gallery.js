2ce0fbd68faa980717653c986c3f1ab519b93ec3
timothy.cao <timothy.cao@bytedance.com>
2025-06-19 23:08:35 -0700

    margin spacing

--- src/pages/blogs/Gallery.js
+++ src/pages/blogs/Gallery.js
@@ -19,15 +19,13 @@ const Gallery = () => {
   const handleOpen = (image) => setSelectedImage(image);
   const handleClose = () => setSelectedImage(null);
 
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
-      <div className="w-full max-w-5xl">
-        <div className="space-y-4 text-left mb-12 mt-12 ml-4">
-          <h1 className="text-5xl font-bold">Gallery</h1>
-          <p className="text-lg text-gray-300 max-w-2xl leading-relaxed">
-            Looking back on 2024
-          </p>
-        </div>
+return (
+    <div className="min-h-screen bg-gray-900 text-white px-4">
+      <div className="max-w-5xl mx-auto space-y-8 mt-24 mb-12">
+        <h1 className="text-5xl font-bold mb-8 mt-6">Gallery</h1>
+        <p className="text-lg text-gray-300 mb-12 leading-relaxed">
+          Looking back on 2024
+        </p>
 
         <Box sx={{ mt: 4, p: 2 }}>
           <ImageList variant="masonry" cols={4} gap={12}>
