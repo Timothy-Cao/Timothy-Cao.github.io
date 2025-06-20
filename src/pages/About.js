ccac150cf7b87d002cdfb836d477747494643144
timothy.cao <timothy.cao@bytedance.com>
2025-06-19 22:37:43 -0700

    formatting

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,9 +9,9 @@ const About = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-900 text-white p-6">
-      <div className="max-w-5xl mx-auto space-y-8">
-      <h1 className="text-5xl font-bold mb-4 ml-4">About</h1>
+  <div className="min-h-screen bg-gray-900 text-white px-4">
+    <div className="max-w-5xl mx-auto space-y-8 mt-24 mb-12">
+        <h1 className="text-5xl font-bold mb-12 px-4">About</h1>
         <div className="text-left">
         </div>
 
