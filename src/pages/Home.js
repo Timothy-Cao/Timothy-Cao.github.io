98b8317fef244dca77b468aff9fef7fe52dbd440
timothy.cao <timothy.cao@bytedance.com>
2025-06-19 18:28:55 -0700

    full length titles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
-const rotatingRoles = ["Software Eng", "Cyber Sec", "Fullstack Dev"];
+const rotatingRoles = ["Software Engineer", "Cyber Security", "Fullstack Developer"];
 
 const blogs = [
   {
